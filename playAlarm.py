cd7a990d45f4053b3842fee44c40950c38a26908
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-02-08 22:44:58 -0500

    Added a statement to force the device to pair to the speaker before playing the sound

--- playAlarm.py
+++ playAlarm.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import time
-
+import os
 from playsound import playsound
 
+os.system("bluetoothctl connect 74:A7:EA:B0:8D:0E")
 while KeyboardInterrupt:
     playsound('audio.mp3')
     time.sleep(0.5)
