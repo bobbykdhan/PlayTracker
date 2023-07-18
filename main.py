257528e8c9b576cb2425d260bb950db6b5741b91
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-07-18 14:21:46 +0000

    Added better logging functionality

--- main.py
+++ main.py
@@ -32,14 +32,15 @@ async def on_ready():
         if debug:
             send_text(get_database_value('MYNUMBER')[0], "Bot is now online in debug mode.")
         else:
-            send_text(get_database_value('MYNUMBER')[0], f"Bot is now online in live mode using the following numbers: {numbers_used}")
+            send_text(get_database_value('MYNUMBER')[0],
+                      f"Bot is now online in live mode using the following numbers: {numbers_used}")
     else:
         print("Bot ran less then 30 minutes ago. Not sending text message.")
 
     set_database_value('LASTRUN', str(time.time()))
 
-def send_text(number, play_message):
 
+def send_text(number, play_message):
     if time.time() > float(get_database_value("SNOOZE")[0]):
         account_sid = get_database_value('TWILIO_ACCOUNT_SID')[0]
         auth_token = get_database_value("TWILIO_AUTH_TOKEN")[0]
@@ -54,9 +55,6 @@ def send_text(number, play_message):
         print("Sent Text Message to " + str(number))
     else:
         print("Snoozed. Not sending text message.")
-
-
-
 
 
 @bot.event
@@ -85,11 +83,11 @@ async def on_message(message):
         no_play(message.content)
         if message.channel.id == channel_id or debug:
             if bool(float(get_database_value("REGULAR")[0])):
-                send_text(get_database_value('MYNUMBER')[0], str("Regular message from the channel:\n" + message.content))
+                send_text(get_database_value('MYNUMBER')[0],
+                          str("Regular message from the channel:\n" + message.content))
             else:
                 print("Regular messages disabled")
             log_date("messages", message.content, "message_storage")
-
 
 
 def handle_message(message, lotto=False):
@@ -111,20 +109,15 @@ def handle_message(message, lotto=False):
         new_message += " \n 0 DAYS TO EXPIRATION \n Lotto Play so be cautious."
 
     if lotto:
-        log_date("plays", new_message, "lotto_storage")
+        log_play(new_message, True)
     else:
-        log_date("plays", new_message, "play_storage")
-
-
-
-
+        log_play(new_message)
     print(new_message)
 
     if not debug:
         send_multiple_texts(new_message)
     else:
         send_text(get_database_value('MYNUMBER')[0], new_message)
-
 
 
 def send_multiple_texts(message):
@@ -134,31 +127,33 @@ def send_multiple_texts(message):
         print("Sending to " + number)
         send_text(number, message)
 
-def get_current_time():
 
+def get_current_time():
     return datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
 
 def no_play(message):
     print("No play found.")
     print("Regular Message: " + message)
 
+
 @app.get('/')
 async def request(request: Request):
     print(f"Request recieved: {str(request)}")
     return Response(content=f"Request recieved: {str(request)}", media_type="application/xml")
-    
+
+
 @app.get('/sms')
 @app.post('/sms')
 async def chat(From: str = Form(...), Body: str = Form(...)):
-
-    if  "/snooze" in Body.lower() and get_database_value('MYNUMBER')[0] in From:
+    if "/snooze" in Body.lower() and get_database_value('MYNUMBER')[0] in From:
         print("Received a snooze message.")
         response = MessagingResponse()
         time_str = (_ := re.findall(r'\d+', Body.lower())[0] if re.findall(r'\d+', Body.lower()) else 5)
         msg = response.message(f"Snoozed for {time_str} minutes.")
         set_database_value("SNOOZE", str(float(time_str) * 60 + time.time()))
         return Response(content=str(response), media_type="application/xml")
-    elif  "/cancel" in Body.lower() and get_database_value('MYNUMBER')[0] in From:
+    elif "/cancel" in Body.lower() and get_database_value('MYNUMBER')[0] in From:
         print("Received a cancel message.")
         response = MessagingResponse()
         msg = response.message(f"Canceled snooze")
@@ -177,9 +172,10 @@ async def chat(From: str = Form(...), Body: str = Form(...)):
         return Response(content=str(response), media_type="application/xml")
     else:
         print(f"Text from: {From} and contains: {Body}")
-        return  {"message": f"Text from: {From} and contains: {Body}"}
+        return {"message": f"Text from: {From} and contains: {Body}"}
 
-def get_database_value(query,table = "envVars"):
+
+def get_database_value(query, table="envVars"):
     mysql = sql.connect(
         host=os.environ['DBHOST'],
         user=os.environ['DBUSER'],
@@ -196,7 +192,9 @@ def get_database_value(query,table = "envVars"):
         if tuple[0] is not None:
             results.append(tuple[0])
     return results
-def set_database_value(query, value, table = "envVars"):
+
+
+def set_database_value(query, value, table="envVars"):
     mysql = sql.connect(
         host=os.environ['DBHOST'],
         user=os.environ['DBUSER'],
@@ -208,7 +206,9 @@ def set_database_value(query, value, table = "envVars"):
     mycursor.execute(f"UPDATE {table} SET {query} = '{value}' LIMIT 1")
 
     mysql.commit()
-def add_database_value(query, value, table = "envVars"):
+
+
+def add_database_value(query, value, table="envVars"):
     mysql = sql.connect(
         host=os.environ['DBHOST'],
         user=os.environ['DBUSER'],
@@ -221,7 +221,8 @@ def add_database_value(query, value, table = "envVars"):
 
     mysql.commit()
 
-def log_date(query, value, table = "envVars"):
+
+def log_date(query, value, table="envVars"):
     mysql = sql.connect(
         host=os.environ['DBHOST'],
         user=os.environ['DBUSER'],
@@ -235,7 +236,26 @@ def log_date(query, value, table = "envVars"):
     mysql.commit()
 
 
-def remove_database_value(query, value, table = "envVars"):
+def log_play(play, lotto=False):
+    mysql = sql.connect(
+        host=os.environ['DBHOST'],
+        user=os.environ['DBUSER'],
+        password=os.environ['DBPASSWORD'],
+        database=os.environ['DBNAME']
+    )
+    mycursor = mysql.cursor()
+    table = (_ := "PLAY_STORAGE" if debug else "LOTTO_STORAGE")
+
+    ticker, strike_price, contract_direction, contract_price = [line.split(": ")[1] for line in
+                                                                play.strip().split("\n")]
+
+
+    mycursor.execute(f"INSERT INTO {table} (Ticker, Strike Price, Contract Direction, Contract Price,date) VALUES ('{ticker}', '{strike_price}', '{contract_direction}', '{contract_price}','{get_current_time()}')")
+
+    mysql.commit()
+
+
+def remove_database_value(query, value, table="envVars"):
     mysql = sql.connect(
         host=os.environ['DBHOST'],
         user=os.environ['DBUSER'],
@@ -248,23 +268,19 @@ def remove_database_value(query, value, table = "envVars"):
 
     mysql.commit()
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     load_dotenv()
     debug = int(get_database_value('DEBUG')[0])
-    
+
     config = uvicorn.Config("main:app", host="0.0.0.0", port=8080, log_level="info")
     server = uvicorn.Server(config)
     webserver = server.serve()
 
     bot_call = bot.start(get_database_value((_ := "TESTDISCORDAUTH" if debug else "DISCORDAUTH"))[0])
-    
+
     bot_task = asyncio.ensure_future(bot_call)
     server_task = asyncio.ensure_future(webserver)
-    
+
     loop = asyncio.get_event_loop()
     loop.run_forever()
-   
-    
-
-
