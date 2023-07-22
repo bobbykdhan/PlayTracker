0b78de64a5c78f6bf817c5218663c3c3e77a80a1
Bobby Dhanoolal <bobbydhanoolal@gmail.com>
2023-07-22 14:10:22 -0400

    Cleaned up the initialization method

--- main.py
+++ main.py
@@ -177,103 +177,9 @@ async def chat(From: str = Form(...), Body: str = Form(...)):
         print(f"Text from: {From} and contains: {Body}")
         return {"message": f"Text from: {From} and contains: {Body}"}
 
-
-def get_database_value(query, table="envVars"):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-
-    mycursor.execute(f"SELECT {query} FROM {table}")
-
-    result_tuples = mycursor.fetchall()
-    results = []
-    for tuple in result_tuples:
-        if tuple[0] is not None:
-            results.append(tuple[0])
-    return results
-
-
-def set_database_value(query, value, table="envVars"):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-
-    mycursor.execute(f"UPDATE {table} SET {query} = '{value}' LIMIT 1")
-
-    mysql.commit()
-
-
-def add_database_value(query, value, table="envVars"):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-
-    mycursor.execute(f"INSERT INTO {table} ({query}) VALUES ('{value}')")
-
-    mysql.commit()
-
-
-def log_date(query, value, table="envVars"):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-
-    mycursor.execute(f"INSERT INTO {table} ({query},date) VALUES ('{value}','{get_current_time()}')")
-
-    mysql.commit()
-
-
-def log_play(play, lotto=False):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-    table = (_ := "PLAY_STORAGE" if debug else "LOTTO_STORAGE")
-
-    ticker, strike_price, contract_direction, contract_price = [line.split(": ")[1].strip() for line in
-                                                                play.strip().split("\n")[:4]]
-
-
-    mycursor.execute(f"INSERT INTO {table} (Ticker, Strike Price, Contract Direction, Contract Price,date) VALUES ('{ticker}', '{strike_price}', '{contract_direction}', '{contract_price}','{get_current_time()}')")
-
-    mysql.commit()
-
-
-def remove_database_value(query, value, table="envVars"):
-    mysql = sql.connect(
-        host=os.environ['DBHOST'],
-        user=os.environ['DBUSER'],
-        password=os.environ['DBPASSWORD'],
-        database=os.environ['DBNAME']
-    )
-    mycursor = mysql.cursor()
-
-    mycursor.execute(f"DELETE FROM {table} WHERE {query} = '{value}'")
-
-    mysql.commit()
-
-
-if __name__ == "__main__":
+def init():
     load_dotenv()
+    global debug
     debug = int(get_database_value('DEBUG')[0])
 
     config = uvicorn.Config("main:app", host="0.0.0.0", port=8080, log_level="info")
@@ -282,8 +188,12 @@ if __name__ == "__main__":
 
     bot_call = bot.start(get_database_value((_ := "TESTDISCORDAUTH" if debug else "DISCORDAUTH"))[0])
 
-    bot_task = asyncio.ensure_future(bot_call)
-    server_task = asyncio.ensure_future(webserver)
+    asyncio.ensure_future(bot_call)
+    asyncio.ensure_future(webserver)
 
     loop = asyncio.get_event_loop()
     loop.run_forever()
+
+
+if __name__ == "__main__":
+    init()
